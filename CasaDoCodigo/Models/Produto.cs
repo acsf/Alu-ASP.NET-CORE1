a2c1f89d35975a8d2d2e0150f5c2effc4807983d
acsf <a.carlossf@hotmail.com>
2020-10-28 03:21:19 -0300

    Capitulo 4 Modelo de Dados

---
+++ CasaDoCodigo/Models/Produto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Models
+{
+    public class Produto
+    {
+        /// <summary>
+        /// Id do produto
+        /// </summary>
+        public int Id { get; private set; }
+
+        /// <summary>
+        /// Nome do produto
+        /// </summary>
+        public string Nome { get; private set; }
+
+        /// <summary>
+        /// Preço do produto
+        /// </summary>
+        public decimal Preco { get; private set; }
+
+        public Produto(int id, string nome, decimal preco)
+        {
+            this.Id = id;
+            this.Nome = nome;
+            this.Preco = preco;
+        }
+
+    }
+}
